c6b234b0c46dfc973d5995ee2a15ef897f79bd9a
Brandon Sheneman <sheneman23@utexas.edu>
2024-03-15 12:45:13 -0500

    Fixed transform order for FK_space

--- Homework2/Programming Assignment/FK_space.m
+++ Homework2/Programming Assignment/FK_space.m
@@ -6,25 +6,15 @@ function T = FK_space(home_config, screw_list, theta_list)      % inputs are the
     % generate all screw axes in se(3) form
     for i=1:length(screw_list)
         screw = screw_list(:, i);        % select screw axis of column i
-        w = screw(1:3);      % select first three elements of screw axis to form omega vector
-        v = screw(4:6);      % select last three elements of screw axis to form velocity vector
-        
-        % form the skew symmetric matrix of omega
-        w_skew = [
-            0, -w(3), w(2);
-            w(3), 0, -w(1);
-            -w(2), w(1), 0];
 
         % form the screw axis in se(3) form
-        S = [
-            w_skew, v;
-            0, 0, 0, 0];
+        S = screw2mat(screw');
 
         S_list{i} = S;
     end
 
     % compute the forward kinematics
-    for i = 1:length(screw_list)
+    for i = length(screw_list):-1:1     % count down from n to 1
         S = S_list{i};      % use the current se(3) representation of the screw
         theta = theta_list(i);      % use current angle value
 
