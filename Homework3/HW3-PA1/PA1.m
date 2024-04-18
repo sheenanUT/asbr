f9df94b10baaadd095f9948bf29f86f309c07ded
Luis Luna <lunaluism@yahoo.com>
2024-04-18 03:23:56 -0500

    pivot_calibration.m

--- Homework3/HW3-PA1/PA1.m
+++ Homework3/HW3-PA1/PA1.m
@@ -1,1 +1,3 @@
 % [Cs, post_EM, post_opt] = read_output("pa1-debug-a-output1.txt")
+% should be 8
+[b_tip, b_post] = pivot_calibration(get_FDs('e'));
--- Homework3/HW3-PA1/pivot_calibration.m
+++ Homework3/HW3-PA1/pivot_calibration.m
@@ -1,4 +1,4 @@
-function [b_tip, b_post] = pivot_calibration(Fk_list)
+function [b_tip, b_post] = pivot_calibration(Fk)
     Rk = [];
     pk = [];
     Ik = [];
@@ -6,13 +6,18 @@ function [b_tip, b_post] = pivot_calibration(Fk_list)
     % find all Rk and Pk and generate Ik
     for i=1:size(Fk, 3)
         Fk_i = Fk(:, :, i);      % set current transformation
-        Rk_i = Fk_i(1:3, 1:3);      % find rotation matrix of current Fk
-        pk_i = Fk_i(1:3, 4);        % find translation vectro of current Fk
+        Rk_i = Fk(1:3, 1:3);      % find rotation matrix of current Fk
+        pk_i = Fk(1:3, 4);        % find translation vectro of current Fk
 
         Rk = [Rk; Rk_i];        % vertically concatenate
         pk = [pk; pk_i];        % vertically concatenate
         Ik = [Ik; eye(3)];  % generate i number of 3x3 identity matrices vertically
     end
 
-    [b_tip, b_post] = lsqr(Rk-Ik, -pk);
+    % least squares method to solve for b_tip and b_post
+    A = [Rk, -Ik];
+    B = -pk;
+    [x, flag] = lsqr(A, B);
+    b_tip = x(1:3);
+    b_post = x(4:6);
 end
