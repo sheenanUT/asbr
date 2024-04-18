b83bc22258f18e839fbd6535e0b72330056bd00a
Luis Luna <lunaluism@yahoo.com>
2024-04-18 17:07:42 -0500

    modified:   Homework3/HW3-PA1/test.m 	modified:   Homework3/opt_pivot.m

--- Homework3/opt_pivot.m
+++ Homework3/opt_pivot.m
@@ -3,48 +3,52 @@ function [t_g, P_dimple] = opt_pivot(filename_opt, filename_body)
     [ds, ~, ~] = read_calbody(filename_body);
 
     % convert H from optical frame to EM frame
+    Hem = zeros(size(Hopt));
     for i = 1:size(Hopt, 3)
         invFD = pc_reg(Ds(:,:,i), ds);      % transformation matrix from optical frame to EM frame
         Hopt_framei = Hopt(:, :, i);        % current frame of H from optical frame
 
-        Hem_framei = 
+        Hem_framei = Hem(:, :, i);
         for k = 1:size(Hopt_framei, 1)
             Hopt_k_framei = Hopt_framei(k, :)';      % current position in current frame
 
             % perform the transform for current position in current frame
             Hem_k_framei = invFD(1:3, 1:3) * Hopt_k_framei + invFD(1:3, 4);
+            Hem_framei(k, :) = Hem_k_framei;
         end
+        Hem(:, :, i) = Hem_framei;
     end
 
-    % Gj_frame1 = Gs(:, :, 1);     % first frame of Gs
-
-    % % find sum of Gj_1
-    % sumGj_frame1 = zeros(1, size(Gj_frame1, 2));
-    % for i = 1:size(Gj_frame1, 1)
-    %     Gi_frame1 = Gj_frame1(i, :);       % current iteration of Gj_frame(1)
-    %     sumGj_frame1 = sumGj_frame1 + Gi_frame1;
-    % end
+    Hj_frame1 = Hem(:, :, 1);     % first frame of Hs
+    Hs = Hem;
     
-    % G0 = (1/size(Gi_frame1, 1)) * sumGj_frame1;       % compute local probe
-
-    % % find gj for every frame
-    % gj = zeros(size(Gs, [1 2 3]));
-    % for i = 1:size(Gs, 3)
-    %     % generate current frame of gj
-    %     gj_framei=zeros(size(Gs, [1 2]));
-    %     Gj_framei = Gs(:, :, i);       % current frame of Gj
-    %     for j = 1:size(Gj_framei, 1)
-    %         Gi_framei = Gj_framei(j, :);       % current iteration of Gj_frame(i)
-    %         gi_framei = Gi_framei - G0;     % solve for current iteration of gj_frame(i)
-    %         gj_framei(j, :) = gi_framei;
-    %     end
-    %     gj(:, :, i) = gj_framei;        % set current frame of gj
-    % end
+    % find sum of Hj_1
+    sumHj_frame1 = zeros(1, size(Hj_frame1, 2));
+    for i = 1:size(Hj_frame1, 1)
+        Hi_frame1 = Hj_frame1(i, :);       % current iteration of Hj_frame(1)
+        sumHj_frame1 = sumHj_frame1 + Hi_frame1;
+    end
     
-    % FG = zeros([4, 4, size(Gs, 3)]);
-    % for i = 1:size(Gs, 3)
-    %     FG(:, :, i) = pc_reg(Gs(:, :, i), gj(:, :, 1));      % find all transforms and stack them only using frame 1 for gj
-    % end
+    H0 = (1/size(Hi_frame1, 1)) * sumHj_frame1;       % compute local probe
 
-    % [t_g, P_dimple] = pivot_calibration(FG);
+    % find gj for every frame
+    gj = zeros(size(Hs, [1 2 3]));
+    for i = 1:size(Hs, 3)
+        % generate current frame of gj
+        gj_framei=zeros(size(Hs, [1 2]));
+        Hj_framei = Hs(:, :, i);       % current frame of Hj
+        for j = 1:size(Hj_framei, 1)
+            Hi_framei = Hj_framei(j, :);       % current iteration of Hj_frame(i)
+            hi_framei = Hi_framei - H0;     % solve for current iteration of hj_frame(i)
+            hj_framei(j, :) = hi_framei;
+        end
+        hj(:, :, i) = hj_framei;        % set current frame of hj
+    end
+    
+    FH = zeros([4, 4, size(Hs, 3)]);
+    for i = 1:size(Hs, 3)
+        FH(:, :, i) = pc_reg(Hs(:, :, i), hj(:, :, 1));      % find all transforms and stack them only using frame 1 for gj
+    end
+
+    [t_g, P_dimple] = pivot_calibration(FH);
 end
