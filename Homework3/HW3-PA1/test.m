71aa52bd45a02384805781a18ac207a391d820f5
Luis Luna <lunaluism@yahoo.com>
2024-04-18 16:46:51 -0500

    modified

--- Homework3/HW3-PA1/test.m
+++ Homework3/HW3-PA1/test.m
@@ -1,6 +1,11 @@
-[Cs, post_EM, post_opt] = read_output('pa1-debug-a-output1.txt');
-disp(post_EM);
+% [Cs, post_EM, post_opt] = read_output('pa1-debug-a-output1.txt');
+% disp(post_EM);
 
-[t_g, P_dimple] = EM_pivot('pa1-debug-a-empivot.txt');
-t_g
-P_dimple
+% [t_g, P_dimple] = EM_pivot('pa1-debug-a-empivot.txt');
+% t_g
+% P_dimple
+filename_opt = 'pa1-debug-a-optpivot.txt';
+filename_body = "pa1-debug-a-calbody.txt";
+[t_g, P_dimple] = opt_pivot(filename_opt, filename_body);
+% [ds, ~, ~] = read_calbody("pa1-debug-a-calbody.txt");
+% [Ds, Hs] = read_optpivot(filename);
