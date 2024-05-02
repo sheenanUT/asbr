05e6bb547f5b18c89a27e896c50a3718c48b4f46
Luis Luna <lunaluism@yahoo.com>
2024-05-02 14:14:36 -0500

    Changes to be committed: 	deleted:    Homework4/q_desired.m 	new file:   Homework4/q_desired_a.m 	new file:   Homework4/q_desired_b.m

--- Homework4/q_desired.m
+++
@@ -1,5 +0,0 @@
-function qd = q_desired(Tsb, J, p_tip, p_goal)
-    [qd, flag] = lsqr(@(delta_q) opt_distance(Tsb, J, p_tip, p_goal, delta_q), 0);
-
-    
-end
---
+++ Homework4/q_desired_a.m
@@ -0,0 +1,13 @@
+function qd = q_desired_a(Tsb, J, p_tip, p_goal, th_list_current)
+    [A, d] = opt_distance(Tsb, J, p_tip, p_goal);       % generate A and d in (Ax-d) = 0 ---> Ax = d
+
+    C = A;
+    b = d+3;        % set b to d+3 | (Ax-d) <= 3 --> Ax<= d+3
+
+    % find lower and upper bound
+    lower_bound = ([-180; -145; -130; -350; -125; -350] * pi/180) - th_list_current;        % set lower limit of joint angles in radians
+    upper_bound = ([180; 45; 150; 350; 125; 350] * pi/180) - th_list_current;       % set upper limit of joint angles in radians
+
+    % perform least sqaure problem with lower bound and upper bound
+    [qd, flag] = lsqlin(C, d, A, b, [], [], lower_bound, upper_bound);
+end
